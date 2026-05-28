b58d5417d92ebd6fccb0ffda3fa990d8bfd89ef9
vvkostenko <Vomewarp@gmail.com>
2026-05-29 02:48:24 +0900

    Fix generic type name

--- Assets/Scripts/DictionaryHelpers.cs
+++ Assets/Scripts/DictionaryHelpers.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 public class DictionaryHelpers
 {
-    public static void AdjustValue<Key>(ref Dictionary<Key, int> dict, Key k, int v)
+    public static void AdjustValue<TKey>(ref Dictionary<TKey, int> dict, TKey k, int v)
     {
         if (dict.ContainsKey(k))
             dict[k] += v;
